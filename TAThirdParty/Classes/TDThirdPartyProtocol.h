4e8c582f99b6c6c95fa95754e3895967382226cd
wwango <wwango@thinkingdata.cn>
2022-10-09 11:00:59 +0800

    feat: update

--- TAThirdParty/Classes/TDThirdPartyProtocol.h
+++ TAThirdParty/Classes/TDThirdPartyProtocol.h
@@ -5,6 +5,8 @@
 //  Created by wwango on 2022/2/17.
 //
 
+#import <Foundation/Foundation.h>
+
 @protocol TDThirdPartyProtocol <NSObject>
 
 - (void)enableThirdPartySharing:(TAThirdPartyShareType)type instance:(id)instance;
